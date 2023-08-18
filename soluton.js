8227e52ece596f5bb42f60b24bde3d44c9b4b6c8
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-08-18 18:53:22 -0400

    added problems to the file

--- soluton.js
+++ soluton.js
@@ -127,3 +127,33 @@ function toUpperCase(string) {
 
   
 }
+
+// * 6. Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
+// Example string : 'Web Development Tutorial'
+// Expected Output : 'Development'
+
+
+// * 7. Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+// Note : As the letter 'y' can be regarded as both a vowel and a consonant, we do not count 'y' as vowel here.
+// Example string : 'The quick brown fox'
+// Expected Output : 5
+
+
+// * 8. Write a JavaScript function that accepts a number as a parameter and checks whether it is prime or not.
+// Note : A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+// * 9. Write a JavaScript function that accepts an argument and returns the type.
+// Note : There are six possible values that typeof returns: object, boolean, function, number, string, and undefined.
+
+
+//*  10. Write a JavaScript function which returns the n rows by n columns identity matrix.
+
+
+// * 11. Write a JavaScript function that takes an array of numbers and finds the second lowest and second greatest numbers, respectively.
+// Sample array : [1,2,3,4,5]
+// Expected Output : 2,4
+
+
+// * 12. Write a JavaScript function that checks whether a number is perfect.
+// According to Wikipedia : In number theory, a perfect number is a positive integer that is equal to the sum of its proper positive divisors, that is, the sum of its positive divisors excluding the number itself (also known as its aliquot sum). Equivalently, a perfect number is a number that is half the sum of all of its positive divisors (including itself).
+// Example : The first perfect number is 6, because 1, 2, and 3 are its proper positive divisors, and 1 + 2 + 3 = 6. Equivalently, the number 6 is equal to half the sum of all its positive divisors: ( 1 + 2 + 3 + 6 ) / 2 = 6. The next perfect number is 28 = 1 + 2 + 4 + 7 + 14. This is followed by the perfect numbers 496 and 8128.
