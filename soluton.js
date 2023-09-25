aa30dae7e7cb8e952d2ce45f18eada7972cd8576
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-09-25 16:02:14 -0400

    added more function problems

--- soluton.js
+++ soluton.js
@@ -164,16 +164,13 @@ function countVowels(str) {
   }
   return count;
 }
-console.log(countVowels("The quick brown fox"))
+console.log(countVowels("The quick brown fox"));
 
 // * 8. Write a JavaScript function that accepts a number as a parameter and checks whether it is prime or not.
 // Note : A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 
-function primeNumber(num){
-  let prime = 0
-
-  
-
+function primeNumber(num) {
+  let prime = 0;
 }
 
 // * 9. Write a JavaScript function that accepts an argument and returns the type.
@@ -188,3 +185,23 @@ function primeNumber(num){
 // * 12. Write a JavaScript function that checks whether a number is perfect.
 // According to Wikipedia : In number theory, a perfect number is a positive integer that is equal to the sum of its proper positive divisors, that is, the sum of its positive divisors excluding the number itself (also known as its aliquot sum). Equivalently, a perfect number is a number that is half the sum of all of its positive divisors (including itself).
 // Example : The first perfect number is 6, because 1, 2, and 3 are its proper positive divisors, and 1 + 2 + 3 = 6. Equivalently, the number 6 is equal to half the sum of all its positive divisors: ( 1 + 2 + 3 + 6 ) / 2 = 6. The next perfect number is 28 = 1 + 2 + 4 + 7 + 14. This is followed by the perfect numbers 496 and 8128.
+
+/*  * 13. Write a function that accepts a String as and argument
+the function should Capitalize ONLY every other letter in the String
+starting the capitalization at index 0
+the function should then return the converted String 
+*/
+
+function stringCap(str) {}
+
+/*
+
+write a function that takes a string as a argument
+
+the string is suppoesd to be HTML, but all the div elements are missing their closing tags (they have the < and > )
+
+the functions job is to find and close all the divs in the provided HTML String
+
+the function should return the fixed HTML String
+
+ */
