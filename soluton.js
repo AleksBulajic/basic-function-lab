55dc22cca39d9cd46101ced9630fe50a14f7a604
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-09-27 08:56:39 -0400

    spell check

--- soluton.js
+++ soluton.js
@@ -214,8 +214,8 @@ function generateIdentityMatrix(n) {
   return identityMatrix;
 }
 
-console.log(generatesIdentityMatrix(3));
-console.log(generatesIdentityMatrix(4));
+console.log(generateIdentityMatrix(3));
+console.log(generateIdentityMatrix(4));
 
 // * 11. Write a JavaScript function that takes an array of numbers and finds the second lowest and second greatest numbers, respectively.
 // Sample array : [1,2,3,4,5]
