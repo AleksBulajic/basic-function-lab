1d731f948ba7de6dd1dee7a7f1859b609e6f56ad
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-09-28 09:03:38 -0400

    lets see if we see the contribution board

--- soluton.js
+++ soluton.js
@@ -254,14 +254,14 @@ the function should then return the converted String
 
 function stringCap(str) {
   let newStr = "";
-  // for (let i = 0; i < str.length; i++) {
-  //   if (i % 2 === 0) {
-  //     newStr += str[i].toUpperCase();
-  //   } else {
-  //     newStr += str[i].toLowerCase();
-  //   }
-  // }
-  // return newStr;
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 0) {
+      newStr += str[i].toUpperCase();
+    } else {
+      newStr += str[i].toLowerCase();
+    }
+  }
+  return newStr;
 }
 console.log(stringCap("hello there"));
 
