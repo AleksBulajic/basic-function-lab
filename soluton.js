4f9ad18a3dbf9bda33eb89a0e218711d9eca2858
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-19 11:48:12 -0400

    firendsList solved!!

--- soluton.js
+++ soluton.js
@@ -312,19 +312,19 @@ function toCamelCase(str) {
 
 console.log(toCamelCase("the-stealth-warrior"));
 
-
 //Make a program that filters a list of strings and returns a list with only your friends name in it.
 //If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not..
 
-function friend(friends){
- let friendsList = []
+function friend(friends) {
+  let friendsList = [];
 
- for(let i = 0;  i < friends.length; i++){
-   if ( friends[i].length === 4 && isNaN(friends[i])){
-    friendsList.push(friends[i])
-   }
- }
-return friendsList
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].length === 4 && isNaN(friends[i])) {
+      friendsList.push(friends[i]);
+    }
+  }
+  return friendsList;
 }
 
 console.log(friend(["Ryan", "Kieran", "Mark"])); // ["Ryan", "Mark"]
+console.log(friend(["Alek", "Bob", "Ivan", "Jozo", "Marko", "Luka"]));
