08c39de01905cb0431026aa00458d89a14ec2793
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-20 14:51:23 -0400

    phoneNumber solution

--- soluton.js
+++ soluton.js
@@ -328,3 +328,23 @@ function friend(friends) {
 
 console.log(friend(["Ryan", "Kieran", "Mark"])); // ["Ryan", "Mark"]
 console.log(friend(["Alek", "Bob", "Ivan", "Jozo", "Marko", "Luka"]));
+
+
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+function createPhoneNumber(numbers){
+  let phoneNumber = ""
+  
+  for(let i = 0; i < numbers.length; i++){
+    if(i === 0 ){
+      phoneNumber += "("
+    }
+    phoneNumber += numbers[i]
+    if( i === 2 ){
+      phoneNumber += ") "
+    }else if(i === 5 ){
+      phoneNumber += "-"
+    }
+  }
+ return phoneNumber
+}
